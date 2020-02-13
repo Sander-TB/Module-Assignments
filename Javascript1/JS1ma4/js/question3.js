56efb842b169cb192f652a6d697d6fdc5343fc7f
Sander Byrkjeland <sunbyr@gmail.com>
2020-02-13 12:31:36 +0100

    working on question 3
    blah

---
+++ Javascript1/JS1ma4/js/question3.js
@@ -0,0 +1,30 @@
+const baseUrl = "https://api.rawg.io/api/games/4200";
+
+fetch(baseUrl)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(json) {
+		createGameDetails(json);
+	})
+	.catch(function(error) {
+		// document.location.href = "error.html"
+	});
+
+function createGameDetails(json) {
+	const name = document.querySelector("h1");
+	name.innerHTML = json.name;
+
+	const image = document.querySelector(".image");
+
+	const description = document.querySelector(".description");
+	description.innerHTML = json.description;
+
+	const released = document.querySelector(".released");
+	released.innerHTML = json.released;
+
+	const rating = document.querySelector(".rating");
+	rating.innerHTML = json.rating;
+
+	console.dir(image);
+}
